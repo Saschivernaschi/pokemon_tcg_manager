bf59c5c2dd2bb14d5d88e6bac025fb0527c6de0a
Sascha <sascha.hen.ruff@gmail.com>
2024-06-08 14:51:43 +0200

    change FeatureList.txt add TODOs dfs

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,9 @@
 import pokemonAPI.PokemonApiClient;
-import pokemonAPI.objects.PokemonCard;
-import pokemonAPI.objects.SimplifiedPokemonCard;
 import serverManagement.Server;
-
 
 public class Main {
 	public static void main(String[] args) {
 		Server server = new Server();
-		String id = "xy1-1"; //Debug
-		// get PokemonCard + sort attributes of object
- 		try {
-			String resp = PokemonApiClient.sendGetRequest("https://api.pokemontcg.io/v2/cards/" + id);
-			PokemonCard pokemonCard = PokemonApiClient.mapString(resp);
-            SimplifiedPokemonCard simplifiedPokemonCard = new SimplifiedPokemonCard();
-            simplifiedPokemonCard.copyFrom(pokemonCard);
-
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-
-		//server.runServer();
+		server.runServer();
 	}
 }
--- src/serverManagement/HttpHandler.java
+++ src/serverManagement/HttpHandler.java
@@ -6,18 +6,42 @@ import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
+import pokemonAPI.PokemonApiClient;
+import pokemonAPI.objects.PokemonCard;
+import pokemonAPI.objects.SimplifiedPokemonCard;
 
+//TODO: Access API with queries to get cards by name
 public class HttpHandler implements com.sun.net.httpserver.HttpHandler {
-
 	private Charset charset = StandardCharsets.UTF_8;
-
+	private static final String POKEMON_TCP_URL = "https://api.pokemontcg.io/v2/cards/";
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		System.out.println(exchange.getRequestURI());
-
-		String body = "HELLO WORLD!";
 		int status = 200;
+		String exchangeUri = (String.valueOf(exchange.getRequestURI()));
+        String body = null;
+		//TODO: change if conditions and implement all cases to get PokemonCards
+		if ("/cards".startsWith(exchangeUri)){
+
+		} else{
+			switch (exchangeUri){
+				case "/users/registration":
+					break;
+				case "/users/login":
+					break;
+				case "/users/persist":
+					break;
+			}
+		}
+
+		//get PokemonCard
+        try {
+			body = getPokemonCardById(exchange);
+        } catch (Exception e) {
+			status = 404;
+			body = "";
+        }
 
 		byte[] bytes = body.getBytes(charset);
 		int bodyLength = bytes.length;
@@ -33,5 +57,12 @@ public class HttpHandler implements com.sun.net.httpserver.HttpHandler {
 		} finally {
 			exchange.close();
 		}
+	}
+	private static String getPokemonCardById(HttpExchange exchange) throws Exception {
+			String body = PokemonApiClient.sendGetRequest(POKEMON_TCP_URL + String.valueOf(exchange.getRequestURI()));
+			PokemonCard pokemonCard = PokemonApiClient.mapString(body);
+			SimplifiedPokemonCard simplifiedPokemonCard = new SimplifiedPokemonCard();
+			simplifiedPokemonCard.copyFrom(pokemonCard);
+			return new ObjectMapper().writeValueAsString(simplifiedPokemonCard);
 	}
 }
